8b46273d852c8d1f6d491c47a9739843e1b5caf3
holloway-okn <61845306+holloway-okn@users.noreply.github.com>
2020-03-19 11:46:51 -0300

    Aqui yurio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
-function appear(){
+$('.box__unity').click(function(){
     document.querySelector('.main__bigbox').classList.add('main__bigbox--show');
     document.querySelector('.content').classList.add('blur');
-}
+});
 
-function hide(){
+function hide() {
     document.querySelector('.main__bigbox').classList.remove('main__bigbox--show');
     document.querySelector('.content').classList.remove('blur');
+}
+
+function remove() {
+    document.querySelector('.box__unity').classList.add('delete');
 }
